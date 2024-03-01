56f7accaf302520bef3f9011408da58e19512028
Samih Batt <158607296+Samih-codes@users.noreply.github.com>
2024-03-01 18:22:45 +0000

    Added Spinbox Functionality

--- Miles To Km Converter/main.py
+++ Miles To Km Converter/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 def button_clicked():
-    miles = float(input.get())  # Retrieve the value from the Entry widget and convert to float
+    miles = float(spinbox.get())  # Retrieve the value from the Spinbox widget and convert to float
     kilometers = miles * 1.60934  # Convert miles to kilometers
     answer.config(text=f"{kilometers:.2f}")
 
@@ -12,28 +12,29 @@ window.minsize(width=350, height=125)
 window.config(padx=20, pady=20)
 
 # Labels
-equal_to = Label(text="is equal to", font= "Anta")
+equal_to = Label(text="is equal to", font="Anta")
 equal_to.grid(column=0, row=1)
 equal_to.config(padx=10, pady=10)
 
-miles_label = Label(text= "Miles", font= "Anta")
+miles_label = Label(text="Miles", font="Anta")
 miles_label.grid(column=2, row=0)
 miles_label.config(padx=10, pady=10)
 
-Km = Label(text="Km", font= "Anta")
-Km.grid(column=2, row=1)
-Km.config(padx=10, pady=10)
+km_label = Label(text="Km", font="Anta")
+km_label.grid(column=2, row=1)
+km_label.config(padx=10, pady=10)
 
-answer = Label(text="", font= "Anta")
+answer = Label(text="", font="Anta")
 answer.grid(column=1, row=1)
 answer.config(padx=10, pady=10)
+
+# Spinbox
+spinbox = Spinbox(from_=0, to=9999999, width=9)
+spinbox.grid(column=1, row=0)
 
 # Button
 button = Button(text="Calculate", command=button_clicked)
 button.grid(column=1, row=3)
 
-# Mile Entry
-input = Entry(width=9)
-input.grid(column=1, row=0)
-
 window.mainloop()
+
